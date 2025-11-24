c4ccdb7ae97b7651eb9ab483f25c42c6265c75af
Koty <koty08@naver.com>
2025-11-24 15:41:46 +0900

    feat: 투표 관리 페이지 생성 및 라우팅 처리

---
+++ src/pages/admin/votes/VoteManagePage.tsx
@@ -0,0 +1,11 @@
+import VoteManageForm from './VoteManageForm';
+
+const VoteManagePage = () => {
+  return (
+    <div className="flex flex-col gap-[70px]">
+      <VoteManageForm />
+    </div>
+  );
+};
+
+export default VoteManagePage;
