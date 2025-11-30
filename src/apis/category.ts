41eacf4a160555d1d53d881ad01ecb44d5ef18fc
koty08 <koty08@naver.com>
2025-11-30 16:03:17 +0900

    feat: 대회 카테고리 DTO 및 Fetcher 정의

---
+++ src/apis/category.ts
@@ -0,0 +1,22 @@
+import { CategoryDto } from 'types/DTO';
+import apiClient from './apiClient';
+
+export const getAllCategory = async () => {
+  const res = await apiClient.get<CategoryDto[]>('/categories');
+  return res.data;
+};
+
+export const postCategory = async (categoryName: string) => {
+  const res = await apiClient.post('/categories', { categoryName });
+  return res.data;
+};
+
+export const patchCategory = async (categoryId: number, categoryName: string) => {
+  const res = await apiClient.patch(`/categories/${categoryId}`, { categoryName });
+  return res.data;
+};
+
+export const deleteCategory = async (categoryId: number) => {
+  const res = await apiClient.delete(`/categories/${categoryId}`);
+  return res.data;
+};
--- src/types/DTO/index.ts
+++ src/types/DTO/index.ts
@@ -4,6 +4,7 @@ export * from './dashboardDto';
 export * from './teamsLikeDto';
 export * from './voteRateDto';
 export * from './contestsDto';
+export * from './categoryDto';
 
 export type { DashboardTeamResponseDto } from './dashboardDto';
 export type { TeamLikeResponseDto } from './teamsLikeDto';
