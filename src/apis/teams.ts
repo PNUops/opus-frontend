c5edd0cd62f2029839c99d87d032c9dd6ac378c6
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-12-04 05:37:00 +0900

    fix: 불필요한 api 호출 함수 매개변수 제거

--- src/apis/teams.ts
+++ src/apis/teams.ts
@@ -57,7 +57,7 @@ export const patchTeamAward = async (teamId: number, payload: PatchAwardRequestD
   return res.data;
 };
 
-export const patchCustomSortTeam = async (contestId: number, payload: PatchCustomOrderRequestDto) => {
+export const patchCustomSortTeam = async (payload: PatchCustomOrderRequestDto) => {
   const res = await apiClient.patch('/teams/sort/custom', payload);
   return res.data;
 };
