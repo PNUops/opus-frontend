55a481c4099790bd69eaf8d8ae1ec2f861d76b70
redzzzi <myqueen1346@naver.com>
2025-12-28 16:20:15 +0900

    feat: 관리자 페이지 헤더 ui 컴포넌트 정의

--- src/components/ui/admin.tsx
+++ src/components/ui/admin.tsx
@@ -61,3 +61,24 @@ export const AdminPopoverDeleteButton = ({ onDelete }: { onDelete: () => void })
     </button>
   );
 };
+
+interface AdminHeaderProps {
+  title: string;
+  description?: string;
+  buttonLabel?: string;
+  onButtonClick?: () => void;
+}
+
+export const AdminHeader = ({ title, description, buttonLabel, onButtonClick }: AdminHeaderProps) => {
+  return (
+    <div className="flex w-full items-center justify-between">
+      <div className="flex items-center gap-3">
+        <h2 className="text-2xl font-bold">{title}</h2>
+        <h4 className="text-exsm text-midGray">{description}</h4>
+      </div>
+      {buttonLabel && onButtonClick && (
+        <AdminCardCreateButton onClick={onButtonClick}>{buttonLabel}</AdminCardCreateButton>
+      )}
+    </div>
+  );
+};
