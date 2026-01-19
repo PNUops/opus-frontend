6e0eefe65d916f7b78055ca04ea29395a8783eed
redzzzi <myqueen1346@naver.com>
2026-01-20 03:13:41 +0900

    design: 마이페이지 레이아웃 border-b 제거

--- src/pages/account/MyPageLayout.tsx
+++ src/pages/account/MyPageLayout.tsx
@@ -1,4 +1,3 @@
-import { ReactNode } from 'react';
 import LayoutSideBar from '@layout/admin/contest/LayoutSideBar';
 import FullContainerLayout from '@layout/FullContainerLayout';
 
@@ -7,7 +6,6 @@ import { user_sections } from 'constants/sidebar';
 const MyPageLayout = () => {
   return (
     <>
-      <div className="border-b px-8 py-2"></div>
       <div className="flex">
         <LayoutSideBar sections={user_sections} />
         <div className="flex-1 border-l">
