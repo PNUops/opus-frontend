660be1b4fa29a88f58d4b5ebbaf54e77323d67a7
redzzzi <myqueen1346@naver.com>
2026-01-20 02:58:36 +0900

    feat: LayoutSidebar를 마이페이지에서도 사용하기 위해 props 추가

---
+++ src/constants/sidebar.ts
@@ -0,0 +1,27 @@
+export const admin_sections = [
+  {
+    title: '프로젝트',
+    links: [
+      { to: 'projects', label: '프로젝트 관리' },
+      { to: 'team-order', label: '정렬 관리' },
+      { to: 'awards', label: '수상 관리' },
+      { to: 'required-fields', label: '필수 항목 설정' },
+    ],
+  },
+  {
+    title: '대회',
+    links: [
+      { to: 'settings', label: '대회 관리' },
+      { to: 'departments', label: '분과 관리' },
+      { to: 'votes', label: '투표 관리' },
+      { to: 'notices', label: '공지 관리' },
+      { to: 'banners', label: '배너 관리' },
+    ],
+  },
+  {
+    title: '통계',
+    links: [{ to: 'statistics', label: '대회 통계' }],
+  },
+];
+
+export const user_sections = [{ title: '계정', links: [{ to: '/myaccount', label: '계정 관리' }] }];
--- src/layout/admin/contest/LayoutSideBar.tsx
+++ src/layout/admin/contest/LayoutSideBar.tsx
@@ -1,7 +1,14 @@
 import { NavLink } from 'react-router-dom';
 import { cn } from 'utils/classname';
 
-const LayoutSideBar = () => {
+interface LayoutSideBarProps {
+  sections: {
+    title: string;
+    links: { to: string; label: string }[];
+  }[];
+}
+
+const LayoutSideBar = ({ sections }: LayoutSideBarProps) => {
   const baseStyle = 'transition-all hover:text-mainGreen';
   const activeLinkStyle = 'font-semibold text-mainGreen';
   const getLinkClass = ({ isActive }: { isActive: boolean }) => cn(baseStyle, isActive && activeLinkStyle);
@@ -26,29 +33,3 @@ const LayoutSideBar = () => {
   );
 };
 export default LayoutSideBar;
-
-const sections = [
-  {
-    title: '프로젝트',
-    links: [
-      { to: 'projects', label: '프로젝트 관리' },
-      { to: 'team-order', label: '정렬 관리' },
-      { to: 'awards', label: '수상 관리' },
-      { to: 'required-fields', label: '필수 항목 설정' },
-    ],
-  },
-  {
-    title: '대회',
-    links: [
-      { to: 'settings', label: '대회 관리' },
-      { to: 'departments', label: '분과 관리' },
-      { to: 'votes', label: '투표 관리' },
-      { to: 'notices', label: '공지 관리' },
-      { to: 'banners', label: '배너 관리' },
-    ],
-  },
-  {
-    title: '통계',
-    links: [{ to: 'statistics', label: '대회 통계' }],
-  },
-];
