d637afb2ad7922845e33fc501a3a19fce4fcabe2
redzzzi <myqueen1346@naver.com>
2026-01-20 03:00:12 +0900

    feat: AdminContestLayout에 admin_section 데이터 props 받는 구조로 수정

--- src/layout/admin/contest/AdminContestLayout.tsx
+++ src/layout/admin/contest/AdminContestLayout.tsx
@@ -3,12 +3,14 @@ import LayoutTopBar from './LayoutTopBar';
 import LayoutSideBar from './LayoutSideBar';
 import FullContainerLayout from '@layout/FullContainerLayout';
 
+import { admin_sections } from 'constants/sidebar';
+
 const AdminContestLayout = () => {
   return (
     <>
       <LayoutTopBar />
       <div className="flex">
-        <LayoutSideBar />
+        <LayoutSideBar sections={admin_sections} />
         <div className="flex-1 border-l">
           <FullContainerLayout />
         </div>
