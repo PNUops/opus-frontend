dda330b02fb10bfcd171e97e9bf4fbe8853f9501
koty08 <koty08@naver.com>
2026-01-27 18:53:07 +0900

    fix: 대회 리스트 아이템의 링크 영역 AdminCardRow 전체로 변경

--- src/pages/admin/ContestListSection.tsx
+++ src/pages/admin/ContestListSection.tsx
@@ -17,18 +17,20 @@ const ContentListSection = () => {
       </AdminCardTop>
       <div className="flex max-h-[300px] flex-col gap-[15px] overflow-y-auto p-2.5">
         {contests?.map((contest) => (
-          <AdminCardRow key={contest.contestId} className="group rounded-lg hover:bg-blue-100">
-            <div className="flex items-center gap-2.5">
-              <div>{contest.contestName}</div>
-              <div className="bg-lightGray text-midGray rounded-3xl px-2.5 py-0.5 text-[14px]">
-                {contest.categoryName}
+          <Link key={contest.contestId} to={`/admin/contest/${contest.contestId}`}>
+            <AdminCardRow className="group rounded-lg hover:bg-blue-100">
+              <div className="flex items-center gap-2.5">
+                <div>{contest.contestName}</div>
+                <div className="bg-lightGray text-midGray rounded-3xl px-2.5 py-0.5 text-[14px]">
+                  {contest.categoryName}
+                </div>
               </div>
-            </div>
-            <Link to={`/admin/contest/${contest.contestId}`} className="flex items-center gap-2">
-              <div className="text-mainBlue hidden text-xs group-hover:block">대회 설정 페이지로 이동</div>
-              <IoIosArrowForward className="group-hover:fill-mainBlue fill-midGray" />
-            </Link>
-          </AdminCardRow>
+              <div className="flex items-center gap-2">
+                <div className="text-mainBlue hidden text-xs group-hover:block">대회 설정 페이지로 이동</div>
+                <IoIosArrowForward className="group-hover:fill-mainBlue fill-midGray" />
+              </div>
+            </AdminCardRow>
+          </Link>
         ))}
         {contests?.length === 0 && <AdminNoData />}
       </div>
