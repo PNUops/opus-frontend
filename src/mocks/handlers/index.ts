e87fd51078434a2aa1a1a36d70bba42012f7f866
redzzzi <myqueen1346@naver.com>
2026-02-22 20:13:36 +0900

    feat: 뷰어 핸들러 추가로 핸들러 배열 확장

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -3,5 +3,6 @@ import { noticesHandler } from './notices';
 import { signInHandlers } from './sign-in';
 import { signUpHandlers } from './sign-up';
 import { teamsHandlers } from './teams';
+import { viewerHandlers } from './viewer';
 
-export const handlers = [...contestsHandler, ...noticesHandler];
+export const handlers = [...contestsHandler, ...noticesHandler, ...viewerHandlers];
