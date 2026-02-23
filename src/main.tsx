deb4aeee454f7d1f6e35048610dabb2c4466d33c
koty08 <koty08@naver.com>
2026-02-23 13:40:50 +0900

    chore: msw 실행 커밋된 부분 롤백

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import { RouterProvider } from 'react-router-dom';
 import AppRoutes from '@route/AppRoutes';
 
 if (process.env.NODE_ENV === 'development') {
-  worker.start();
+  // await worker.start();
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
