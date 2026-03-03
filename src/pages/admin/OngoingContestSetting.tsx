a79c521e060cd13c9f1a45a2840d9d69e3efcbf3
koty08 <koty08@naver.com>
2026-03-03 20:19:14 +0900

    feat: 어드민 메인 - 진행 중인 대회 설정 컴포넌트에 QueryWrapper 적용, 대회 선택 컴포넌트로 변경

--- src/pages/admin/OngoingContestSetting.tsx
+++ src/pages/admin/OngoingContestSetting.tsx
@@ -1,43 +1,23 @@
-import { useQuery } from '@tanstack/react-query';
-import { useEffect, useState } from 'react';
-import { IoIosArrowDown } from 'react-icons/io';
-import { ContestResponseDto } from 'types/DTO';
-import { contestOption } from 'queries/contests';
+import { useState } from 'react';
 import { ContestSlots } from './ContestSlots';
+import ContestSelect from './ContestSelect';
+import QueryWrapper from 'providers/QueryWrapper';
 
 const OngoingContestSetting = () => {
-  const [selectedContest, setSelectedContest] = useState<ContestResponseDto>();
-  const { data: contests } = useQuery(contestOption());
+  const [selectedContest, setSelectedContest] = useState<string>('');
 
-  useEffect(() => {
-    if (!selectedContest && contests) setSelectedContest(contests[0]);
-  }, [contests, selectedContest]);
-
-  const onSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const contest = contests?.find((contest) => contest.contestName === e.target.value);
-    if (contest) setSelectedContest(contest);
-  };
+  const onChangeContest = (contestId: string) => setSelectedContest(contestId);
 
   return (
     <div className="flex flex-col gap-5">
       <h2 className="text-2xl font-bold">진행 중인 대회 설정</h2>
       <div className="flex flex-wrap items-center justify-between gap-2.5">
-        <div className="relative inline-block w-64">
-          <select
-            onChange={onSelectChange}
-            className="border-subGreen cursor-pointer appearance-none rounded border-b-2 bg-white px-4 py-2 pr-10 text-nowrap focus:outline-none"
-          >
-            {contests?.map((contest: ContestResponseDto) => (
-              <option key={contest.contestId} value={contest.contestName}>
-                {contest.contestName}
-              </option>
-            ))}
-          </select>
-          <IoIosArrowDown className="text-mainGreen pointer-events-none absolute top-1/2 right-7 -translate-y-1/2 text-lg" />
-        </div>
-        <div className="flex flex-wrap gap-5">
-          <ContestSlots contests={contests} selectedContest={selectedContest} />
-        </div>
+        <QueryWrapper loadingStyle="h-10 my-0 rounded-sm" errorStyle="h-10 flex-wrap">
+          <ContestSelect contestId={selectedContest} onChange={onChangeContest} />
+          <div className="flex flex-wrap gap-5">
+            <ContestSlots selectedId={selectedContest} />
+          </div>
+        </QueryWrapper>
       </div>
     </div>
   );
