ef18715f89fdb4fd6fdf8cd925a7dff239924b98
redzzzi <myqueen1346@naver.com>
2026-03-21 12:12:44 +0900

    feat: 마이페이지 레이아웃 구조 개선 및 CSS 클래스 수정

--- src/layout/me/MyPageLayout.tsx
+++ src/layout/me/MyPageLayout.tsx
@@ -4,14 +4,14 @@ import myPageSidebarData from '@layout/me/myPageSidebarData';
 
 const MyPageLayout = () => {
   return (
-    <>
-      <div className="flex">
+    <div className="flex w-full flex-col md:flex-row">
+      <div className="w-full md:w-auto md:max-w-65 md:min-w-55">
         <LayoutSideBar sections={myPageSidebarData} />
-        <div className="flex-1 border-l">
-          <FullContainerLayout />
-        </div>
       </div>
-    </>
+      <div className="flex-1 md:border-t-0 md:border-l">
+        <FullContainerLayout />
+      </div>
+    </div>
   );
 };
 
