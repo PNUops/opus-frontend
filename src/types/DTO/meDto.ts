ebdc8ede66480e06513c4578d345e39e27764c0b
redzzzi <myqueen1346@naver.com>
2026-03-21 18:51:53 +0900

    feat: MyVoteDto 및 MyLikeDto 인터페이스를 export로 변경하고 thumbnailUrl 필드 제거

--- src/types/DTO/meDto.ts
+++ src/types/DTO/meDto.ts
@@ -9,22 +9,28 @@ export interface MyProjectDto {
   awards: AwardDto[];
 }
 
-interface MyVoteDto {
+export interface MyVoteDto {
   contestId: number;
   contestName: string;
   teamId: number;
   teamName: string;
   projectName: string;
-  thumbnailUrl: string | null;
 }
 
-interface MyLikeDto {
+export interface MyLikeDto {
+  contestId: number;
+  contestName: string;
   teamId: number;
   teamName: string;
-  contestId: number;
   projectName: string;
-  thumbnailUrl: string | null;
-  contestName: string;
+}
+
+interface MyLikeGridPagination {
+  content: MyLikeDto[];
+  totalElements: number;
+  totalPages: number;
+  currentPage: number;
+  size: number;
 }
 
 export type GetMyProjectsResponseDto = MyProjectDto[];
@@ -32,4 +38,14 @@ export type GetMyProjectsResponseDto = MyProjectDto[];
 export type GetMyVotesResponseDto = MyVoteDto[];
 
 export type GetMyLikesPreviewResponseDto = MyLikeDto[]; // INFO: 미리보기는 3개까지만 반환
-export type GetMyLikesResponseDto = MyLikeDto[];
+export type GetMyLikesResponseDto = MyLikeGridPagination;
+
+export type GetMyLikesParamsDto = {
+  sort?: 'latest' | 'oldest';
+  startDate?: string;
+  endDate?: string;
+  category?: string;
+  contestId?: number;
+  page: number;
+  size: number;
+};
