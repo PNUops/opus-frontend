b6763634e7a00de218443e78d8af8cbd8beec706
koty <koty0833@gmail.com>
2026-03-31 16:44:59 +0900

    feat: 대회 페이지에 대회 공지사항 추가

---
+++ src/pages/contest/ContestNoticeList.tsx
@@ -0,0 +1,20 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { NoticeList, NoticeListItem, NoticeListNoData } from '@components/notice';
+import { useContestIdOrRedirect } from 'hooks/useId';
+import { contestNoticeOption } from 'queries/notices';
+
+const ContestNoticeList = () => {
+  const contestId = useContestIdOrRedirect();
+  const { data: notices } = useSuspenseQuery(contestNoticeOption(contestId));
+
+  return (
+    <NoticeList>
+      {notices.length === 0 && <NoticeListNoData />}
+      {notices.slice(0, 3).map((notice) => (
+        <NoticeListItem key={notice.noticeId} {...notice} />
+      ))}
+    </NoticeList>
+  );
+};
+
+export default ContestNoticeList;
--- src/pages/contest/ContestPage.tsx
+++ src/pages/contest/ContestPage.tsx
@@ -1,7 +1,10 @@
 import TeamCardGrid from '@pages/contest/TeamCardGrid';
+import { NoticeListSkeleton } from '@components/notice';
 import useTeamList from 'hooks/useTeamList';
 import useContestName from 'hooks/useContestName';
 import { useContestIdOrRedirect } from 'hooks/useId';
+import QueryWrapper from 'providers/QueryWrapper';
+import ContestNoticeList from './ContestNoticeList';
 
 const ContestPage = () => {
   const contestId = useContestIdOrRedirect();
@@ -12,6 +15,9 @@ const ContestPage = () => {
   return (
     <div className="flex flex-col gap-8">
       <h3 className="lg:text-title text-2xl font-bold">{contestName ?? ''}</h3>
+      <QueryWrapper loadingFallback={<NoticeListSkeleton />} errorStyle="h-36 rounded-xl shadow-md">
+        <ContestNoticeList />
+      </QueryWrapper>
       <TeamCardGrid teams={teams} isLoading={isLoading} isError={isError} />
     </div>
   );
