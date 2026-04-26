302028d2d82fba5a34eb54f2cee77022d391ef6b
redzzzi <myqueen1346@naver.com>
2026-04-26 17:10:03 +0900

    improve: CTA 드러나도록 미로그인 프롬프트 ui 개선

--- src/pages/project-viewer/CommentSection/CommentSection.tsx
+++ src/pages/project-viewer/CommentSection/CommentSection.tsx
@@ -10,17 +10,9 @@ interface CommentSectionProps {
 
 const CommentSection = ({ teamId }: CommentSectionProps) => {
   const { isSignedIn } = useAuth();
-  const navigate = useNavigate();
 
   if (!isSignedIn) {
-    return (
-      <button
-        onClick={() => navigate('/signin')}
-        className="bg-whiteGray text-midGray text-exsm flex h-[100px] w-full items-center justify-center rounded hover:cursor-pointer sm:text-sm"
-      >
-        댓글 작성 및 열람은 로그인이 필요해요.
-      </button>
-    );
+    return <CommentSigninPrompt />;
   }
 
   return (
@@ -35,3 +27,17 @@ const CommentSection = ({ teamId }: CommentSectionProps) => {
 };
 
 export default CommentSection;
+
+const CommentSigninPrompt = () => {
+  const navigate = useNavigate();
+
+  return (
+    <button
+      onClick={() => navigate('/signin')}
+      className="bg-whiteGray text-midGray text-exsm flex h-30 w-full flex-col items-center justify-center gap-4 rounded hover:cursor-pointer sm:h-25 sm:flex-row sm:text-sm"
+    >
+      <p className="text-gray-500">댓글 작성 및 열람은 로그인 후 이용할 수 있어요.</p>
+      <span className="text-mainBlue hover:font-semibold hover:underline">로그인하기</span>
+    </button>
+  );
+};
