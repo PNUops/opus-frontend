680948596dc359bc9843ebde83d7cae5a7dd4a08
redzzzi <myqueen1346@naver.com>
2026-05-17 19:35:00 +0900

    feat: main msw worker 제거 - main 브랜치만 해당

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-import { worker } from '@mocks/browsers';
 import { QueryClientProvider } from '@tanstack/react-query';
 import queryClient from '@stores/queryClient';
 import { RouterProvider } from 'react-router-dom';
 import AppRoutes from '@route/AppRoutes';
 
-if (import.meta.env.MODE === 'development') {
-  //   await worker.start();
-}
+const enableMocking = async () => {
+  if (!import.meta.env.DEV || import.meta.env.VITE_ENABLE_MOCKS !== 'true') {
+    return;
+  }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={AppRoutes()} />
-    </QueryClientProvider>
-  </React.StrictMode>,
-);
+  const mockWorkerPath = '/src/mocks/browsers.ts';
+  const { worker } = await import(/* @vite-ignore */ mockWorkerPath);
+  await worker.start();
+};
+
+void enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={AppRoutes()} />
+      </QueryClientProvider>
+    </React.StrictMode>,
+  );
+});
