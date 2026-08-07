93f03c246acae1a7b7f963355cdfdb27aa4d686c
redzzzi <myqueen1346@naver.com>
2026-08-07 14:16:41 +0900

    chore: 미사용 컴포넌트 제거

--- src/pages/main/OpusClock.tsx
+++ src/pages/main/OpusClock.tsx
@@ -7,7 +7,7 @@ const dateFormatter = new Intl.DateTimeFormat('ko-KR', {
   year: 'numeric',
   month: '2-digit',
   day: '2-digit',
-  weekday: 'short',
+  weekday: 'long',
 });
 
 const timeFormatter = new Intl.DateTimeFormat('ko-KR', {
